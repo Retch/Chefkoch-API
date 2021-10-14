bd0dffee009209f73ae1a8f030ac3d5d0a6657a3
Retch <retch100@gmail.com>
2021-10-14 13:19:07 +0200

    added upload to firestore

---
+++ firebase.py
@@ -0,0 +1,41 @@
+from chefkoch import ChefKochAPI, DataParser
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("credentials.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+if __name__ == '__main__':
+    categories = ChefKochAPI.get_categories()
+    amount = 5
+    catname = "Hauptspeise"
+
+    category = None
+
+    for cat in categories:
+        if cat.title == catname:
+            category = cat
+            break
+
+    recipes = ChefKochAPI.parse_recipes(category, amount)
+
+    firestre = db.collection(category.title).get()
+    
+    for recipe in recipes:
+        if recipe.id not in firestre:
+            db.collection(category.title).document(recipe.id).set(
+                {
+                    "id": int(recipe.id),
+                    "name": recipe.name,
+                    "img": recipe.img,
+                    "categories": [cat for cat in recipe.categories],
+                    "description": recipe.descrip,
+                    "hints": [hint for hint in recipe.hints],
+                    "ingredients": [ingredient.__dict__ for ingredient in recipe.ingredients],
+                }
+            )
+    print("Done")
